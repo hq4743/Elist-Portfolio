eb85eda89967fb7d602810c280d5c06dab739587
Swathi Bhaskaran <147574715+hq4743@users.noreply.github.com>
2026-02-11 18:59:32 -0500

    Update Elist.sql

--- Elist.sql
+++ Elist.sql
@@ -4,8 +4,6 @@ SELECT  FROM `elistcore.core.customers` LIMIT 1000
 select min(purchase_ts)as earliest,max(purchase_ts)as latest from
 core.orders
 
-
-
 -- 2. What is the average order value for purchases made in USD? What about average order value for purchases made in USD in 2019?
 
 
@@ -22,3 +20,34 @@ select distinct(product_name),purchase_platform from core.orders where currency 
 -- 5. What are the first 10 countries in the North American region, sorted in descending alphabetical order?
 
 select * from core.geo_lookup where region = 'NA' order by 1 desc limit 10;
+
+--6. What is the total number of orders by shipping month, sorted from most recent to oldest?
+select date_trunc(ship_ts, month) as month,
+  count(distinct order_id) as order_count
+from core.order_status
+group by 1
+order by 1 desc;
+
+--7.What is the average order value by year? Can you round the results to 2 decimals?
+select extract(year from purchase_ts) as year, 
+  round(avg(usd_price),2) as aov
+from core.orders
+group by 1
+order by 1;
+
+--8.Create a helper column is_refund  in the order_status  table that returns 1 if there is a refund, 0 if not. Return the first 20 records.
+select *, 
+  case when refund_ts is not null then 1 else 0 end as is_refund
+from core.order_status
+limit 20;
+
+ --9.Return the product IDs and product names of all Apple products.
+select distinct product_id,
+  product_name
+from core.orders
+where product_name like '%Apple%'
+or product_name = 'Macbook Air Laptop';
+--10.Calculate the time to ship in days for each order and return all original columns from the table.
+select *, 
+  date_diff(ship_ts,purchase_ts, day) as days_to_ship
+from core.order_status;
